00fedcc36116288e44402ce0bba46d534725c018
= <=>
2025-10-05 06:08:29 +0900

    :sparkles: setup tailwind / daisy ui / icons

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold undeline text-red-400">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary">
+      View Map <Icon name="tabler:map" size="32" />
+    </button>
   </div>
 </template>
