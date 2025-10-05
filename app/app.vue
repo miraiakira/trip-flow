a51aec19c8789a01a4548ecce7679db5f26e8d4d
miraiakira <125364592+miraiakira@users.noreply.github.com>
2025-10-05 16:48:25 +0900

    Tl 11 (#50)

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold undeline text-red-400">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" size="32" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
