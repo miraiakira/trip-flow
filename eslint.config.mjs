8b35fa7054d257671bb1f71074cf633989114fca
= <=>
2025-10-10 00:06:41 +0900

    add db schemas

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ export default withNuxt(antfu(
       semi: true,
       quotes: "double",
     },
-    ignores: [".pnpm-store/**"],
+    ignores: [".pnpm-store/**", "**/migrations/*"],
   },
   {
     rules: {
