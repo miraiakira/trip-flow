0bee0b0366a821b252af3b7b303dab2114f93ecc
= <=>
2025-11-01 15:05:33 +0900

    feat: add unique contraint to location slug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "drizzle-kit";
 import env from "./app/lib/env";
 
 export default defineConfig({
-  out: "./app/lib/migrations",
+  out: "./app/lib/db/migrations",
   schema: "./app/lib/db/schema/index.ts",
   casing: "snake_case",
   dialect: "turso",
--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,5 +1,13 @@
+import type { DrizzleError } from "drizzle-orm";
+
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
+
 import db from "~/lib/db";
 import { InsertLocation, location } from "~/lib/db/schema";
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -25,11 +33,51 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const [created] = await db.insert(location).values({
-    ...result.data,
-    slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
-    userId: event.context.user.id, // 这里是 number，不需要再转换
-  }).returning();
+  const existingLocation = await db.query.location.findFirst({
+    where: and(
+      eq(location.userId, event.context.user.id),
+      eq(location.name, result.data.name),
+    ),
+  });
 
-  return created;
+  if (existingLocation) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "Location already exists",
+    }));
+  }
+
+  let slug = slugify(result.data.name);
+  let existing = !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+
+  while (existing) {
+    const id = nanoid();
+    const idSlug = `${slug}-${id}`;
+    existing = !!(await db.query.location.findFirst({
+      where: eq(location.slug, idSlug),
+    }));
+    if (!existing) {
+      slug = idSlug;
+    }
+  }
+
+  try {
+    const [created] = await db.insert(location).values({
+      ...result.data,
+      slug,
+      userId: event.context.user.id, // 这里是 number，不需要再转换
+    }).returning();
+
+    return created;
+  }
+  catch (e) {
+    const error = e as DrizzleError;
+    console.error("Insert Error: ", error.message);
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "db error",
+    }));
+  }
 });
